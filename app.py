bb784e3dc0ee94615e53d5e65d1dba689faa17e6
Rofiat <oyizakareem9@gmail.com>
2025-06-08 19:51:03 +0100

    added docker comands

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-import pandas as pd 
-import requests
-
-
 print(2+2)
+
+
+
+# url = 'https://wwe'
+
+# response = reguests.get(url)
+# data = response.json()
+# df = pd.DataFrame(data)
+# ...
+
+
+
+# def get_users():
+#     if response.status_code == 200:
+#         response = reguests.get(url)
+#         data = response.json()
+#         df = pd.DataFrame(data)
